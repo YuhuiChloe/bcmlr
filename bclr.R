1b78a1569d1e16242068a9f441ac23bb47e277ec
YuhuiChloe <87951710+YuhuiChloe@users.noreply.github.com>
2024-11-28 22:03:14 -0500

    Update bclr.R

--- bclr.R
+++ bclr.R
@@ -1,8 +1,8 @@
 #Held-out strategy:
-#- Hold out every samples at indices multiple of 3 (thinning = 3) when fitting bclr.  
-#- Use the held-out data to compute AUC. 
+  #- Hold out every samples at indices multiple of 3 (thinning = 3) when fitting bclr.  
+  #- Use the held-out data to compute AUC. 
 
-bcmlr <- function(data, num_iter = 10000, num_warmup = 5000, init_CP = NA, num_CP, thinning, print_outputs = TRUE){
+bclr <- function(data,  num_iter = 10000, num_warmup = 5000, init_CP = NA, thinning = 1, print_outputs = FALSE){
   
   # a function to center a matrix by a vector of means
   center <- function(x, m){
@@ -12,15 +12,13 @@ bcmlr <- function(data, num_iter = 10000, num_warmup = 5000, init_CP = NA, num_C
     return(x)
   }
   
+  n = dim(data)[1] # length of the entire series
   X = as.matrix(data)
-  if (anyNA(init_CP) == FALSE && length(init_CP) != num_CP){
-    stop("initial changepoints must have the same length with the number of changepoints")
-  }
-  if (length(thinning) > 2 || thinning < 1){
-    stop("thinning must be a positive integer no less than 1")
-  }
   
-  n = nrow(X)
+  if (length(thinning) > 2 || thinning < 0 ){
+      print("thinning must be a positive integer")
+  }
+
   if (thinning != 1){
     X_auc = X[seq_len(n) %% thinning == 0, ] # rows of X selected to calculate AUC
     X = X[seq_len(n) %% thinning != 0, ] # rows of X selected to fit bclr 
@@ -28,16 +26,18 @@ bcmlr <- function(data, num_iter = 10000, num_warmup = 5000, init_CP = NA, num_C
     X = center(X, m) # center the subseries used to fit the model with its column means
     X_auc = center(X_auc, m) # center the held-out samples with the same column means 
   }else{
-    m = colMeans(X)
-    X = center(X,m)
+    m = colMeans(X) 
+    X = center(X, m)
     X_auc = X
   }
   
-  N = dim(X)[1] # Sample size of the subseries used to fit the data
-  p = dim(X)[2] # data dimension 
+  N = dim(X)[1] # Sample size for fitting the model
+  p = dim(X)[2] # data dimension
   
   
-  ### Functions used for both cases (single CP and multiple CPs) ####
+  ##############################################################
+  # Functions used for both cases (single CP and multiple CPs) #
+  ##############################################################
   
   # https://arxiv.org/pdf/2205.04997
   # https://gregorygundersen.com/blog/2020/02/09/log-sum-exp/
@@ -85,320 +85,137 @@ bcmlr <- function(data, num_iter = 10000, num_warmup = 5000, init_CP = NA, num_C
     
     return(auc)
   }
-
-  #### Utility functions and functions for Gibbs sampling, Metropolis-Hastings check, and parallel computing ####
-  
-  # 1-1 Correspondence
-  # A function that converts a change point vector to a response matrix with 
-  # the change point structure
-  kappa_to_Y <- function(kappa, N, L){
-    # L <- length(kappa)
-    class <- rep(1, kappa[1])
-    for(j in 2:L){
-      class <- c(class, rep(j, kappa[j] - kappa[j-1]))
-    }
-    class <- c(class, rep(L+1, N - kappa[L]))
-    class <- as.factor(class)
-    Y <- model.matrix(~ -1 + class, data=class)
-    return(Y)
-  } 
-  
-  # Use this function when calculating the log target in MH steps. 
-  # A function that converts kappa (with length L) to L+1 penalization ratios. 
-  kappa_to_alpha <- function(kappa, N, L){
-    alpha = rep(0, times = L+1)
-    for (j in 1:(L+1)){
-      if (j == 1){
-        alpha[j] = N/kappa[1]
-      }else if(j == L+1){
-        alpha[j] = N/(N-kappa[L])
-      }else{
-        alpha[j] = N/(kappa[j] - kappa[j-1])
-      }
-    }
-    return(alpha)
-  }
   
   
-  #### A function that updates \beta, \kappa, and  \omega per iteration  ########
-  GS_update <- function(temper, beta, kappa, omega, data, X, X_auc, N, L, J, m0, V0inv){
-    
-    # update Y, delta
-    Y = kappa_to_Y(kappa, N, L)
-    delta = temper*(Y-1/2)
   
-    ### Update beta and omega #####
-    for(j in 1:(J-1)){
-      
-      # Polson et al. (2013) describes or implies the existence of matrices 
-      # C and eta. We compute these here. Can potentially use log-sum-exp trick
-      # as described here: 
-      # https://gregorygundersen.com/blog/2020/02/09/log-sum-exp/
-      c_j = log(rowSums(exp(X %*% beta[,-j])))
-      eta_j = X %*% beta[,j] - c_j 
-      
-      for(i in 1:N){ 
-        omega[i,j] <- rpg(num=1, h = temper, z = eta_j[i]) # update omega
-      }
-      
-      # V_j <- solve(t(X) %*% diag(omega[,j]) %*% X + V0inv[,,j])
-      # V_j <- chol2inv(chol(t(X) %*% diag(omega[,j]) %*% X + V0inv[,,j]))
-      # m_j <- V_j %*% (t(X) %*% (delta[,j] + diag(omega[,j]) %*% c_j) + V0inv[,,j] %*% m0[,j])
-      # beta[,j] = mvrnorm(n = 1, mu = m_j, Sigma = V_j)  # update beta
-      Z <- matrix(rnorm(p), p, 1) # A p-vector of standard normals
-      U <- chol(crossprod(x = X, y = diag(omega[,j]) %*% X) + V0inv[,,j]) # Upper Cholesky factor of the inverse of V_j 
-      beta[,j] <- chol2inv(U) %*% (t(X) %*% (delta[,j] + diag(omega[,j]) %*% c_j) + V0inv[,,j] %*% m0[,j]) + backsolve(U, Z)
-    }
-
-    
-    #### Update kappa (and Y, which is a function of kappa) #### 
-    
-    # Create the P matrix
-    # P[i,j] is equal to p_{i,j} from the supplement of Polson et al. (2013). 
-    Phi_exp <- exp(X %*% beta) # Element-wise exponent of Phi matrix from supplement
-    P <- t(apply(Phi_exp, 1, function(x) x/sum(x))) # Normalize the rows of Phi_exp to get P
-    for(j in 1:(J-1)){
-      
-      # Now calculate the range of values that kappa[j] can take under its full 
-      # conditional distribution and the vector prob_vec, which is defined 
-      # such that prob_vec[k] is the log of the conditional probability that 
-      # kappa[j] = range[k] given all the other data and parameters. 
-      if(j==1){
-        range <- 1:(kappa[j+1]-1)
-        lr <- length(range)
-        log_prob_vec_prop <- rep(0, lr) 
-        for(l in 1:lr){
-          log_prob_vec_prop[l] <- temper*(sum(log(P[1:range[l],j])) + sum(log(P[(range[l]+1):kappa[j+1],j+1])))  
-          - length(1:range[l])*log(length(1:range[l])) 
-          - length((range[l]+1):kappa[j+1])*log(length((range[l]+1):kappa[j+1]))
-        }
-        #prob_vec <- exp(log_prob_vec_prop)/sum(exp(log_prob_vec_prop))
-        prob_vec <- exp(log_prob_vec_prop - logsumexp(log_prob_vec_prop)) 
-        
-      } else if(j==L){
-        range <- (kappa[j-1]+1):(N-1)
-        lr <- length(range)
-        log_prob_vec_prop <- rep(0, lr)
-        for(l in 1:lr){
-          log_prob_vec_prop[l] <- temper*(sum(log(P[(kappa[j-1]+1):range[l],j])) + sum(log(P[(range[l]+1):N,j+1])))
-          - length((kappa[j-1]+1):range[l])*log(length((kappa[j-1]+1):range[l]))
-          - length((range[l]+1):N)*log(length((range[l]+1):N))
-        }
-        #prob_vec <- exp(log_prob_vec_prop)/sum(exp(log_prob_vec_prop))
-        prob_vec <- exp(log_prob_vec_prop - logsumexp(log_prob_vec_prop)) 
-      } else{
-        range <- (kappa[j-1]+1):(kappa[j+1]-1)
-        lr <- length(range)
-        log_prob_vec_prop <- rep(0, lr) 
-        for(l in 1:lr){
-          log_prob_vec_prop[l] <- temper*(sum(log(P[(kappa[j-1]+1):range[l],j])) + sum(log(P[(range[l]+1):kappa[j+1],j+1]))) 
-          - length((kappa[j-1]+1):range[l])*log(length((kappa[j-1]+1):range[l])) 
-          - length((range[l]+1):kappa[j+1])*log(length((range[l]+1):kappa[j+1]))
-        }
-        #prob_vec <- exp(log_prob_vec_prop)/sum(exp(log_prob_vec_prop))
-        prob_vec <- exp(log_prob_vec_prop - logsumexp(log_prob_vec_prop)) 
-      }
-      
-      if(lr > 1){
-        kappa[j] <- sample(x=range, size=1, prob=prob_vec) # update kappa
-      }
-    }
-    
-    ######### Compute AUC ######
-    # "ratios" are the probabilities of a subject belonging to 
-    # class j+1 other than class j
-    X_all = as.matrix(data)
-    Phi_exp_all <- exp(X_all %*% beta) # Element-wise exponent of Phi matrix from supplement
-    P_all <- t(apply(Phi_exp_all, 1, function(x) x/sum(x))) # Normalize the rows of Phi_exp to get P
-    
-    n = dim(data)[1]
-    idx_all = 1:n
-    if (thinning ==1){
-      idx_fit = idx_all
-    }else{
-      idx_fit = idx_all[idx_all%%thinning != 0]
-    }
-    original_kappa = idx_fit[kappa]
-    Y_all <- kappa_to_Y(original_kappa, n, L)
-    auc_values = rep(0, times = J-1)
-    
-    for (j in 1:(J-1)){
-      if(j == 1){
-        rg = 1:(original_kappa[j+1]-1)
-        lg = length(rg)
-        ratios = P_all[rg,j+1]/(P_all[rg,j] + P_all[rg,j+1])
-        ratios = ratios[seq_len(lg) %% thinning == 0]
-        Y_auc = Y_all[rg, j + 1]
-        Y_auc = Y_auc[seq_len(lg) %% thinning == 0]
-        auc_values[j] = calculate_roc_auc(Y_auc, ratios)
-      }else if (j == J-1){
-        rg = (original_kappa[j-1]+1):(n-1)
-        lg = length(rg)
-        ratios = P_all[rg,j+1]/(P_all[rg,j] + P_all[rg, j+1])
-        ratios = ratios[seq_len(lg) %% thinning == 0]
-        Y_auc = Y_all[rg, j + 1]
-        Y_auc = Y_auc[seq_len(lg) %% thinning == 0]
-        auc_values[j] = calculate_roc_auc(Y_auc, ratios)
-      }else{
-        rg = (original_kappa[j-1]+1):(original_kappa[j+1]-1)
-        lg = length(rg)
-        ratios = P_all[rg,j+1]/(P_all[rg,j] + P_all[rg, j+1])
-        ratios = ratios[seq_len(lg) %% thinning == 0]
-        Y_auc = Y_all[rg, j + 1]
-        Y_auc = Y_auc[seq_len(lg) %% thinning == 0]
-        auc_values[j] = calculate_roc_auc(Y_auc, ratios)
-      }
-    }
-    
-    if (temper == 1){
-      return( list(beta = beta, kappa = kappa, original_kappa = original_kappa, omega = omega, P = P, Auc = auc_values) )
-    }else{
-      return( list(beta = beta, kappa = kappa, original_kappa = original_kappa, omega = omega) )
-    }
-    
-  }
+  ##### SINGLE change point case ######
+  #cat("Fitting ", num_CP, " change points...... \n")
   
-  ##### A function that return a value of the logarithm of the target density kernel ######
-  logtarget <- function(temper, beta, kappa, omega, X, N, L, J, m0, V0inv){
-    
-    # update Y, delta, and alpha
-    Y <- kappa_to_Y(kappa, N, L)
-    delta <- temper*(Y-1/2)
-    alpha <- kappa_to_alpha(kappa, N,L)
-    
-    logkernel = 0 
-    for (j in 1:(J-1)){
-      c_j = log(rowSums(exp(X %*% beta[,-j])))
-      eta_j = X %*% beta[,j] - c_j 
-      z_j = delta[,j]/omega[,j]
-      
-      prior_j = - 0.5*t(beta[,j]-m0[,j])%*%V0inv[,,j]%*%(beta[,j]-m0[,j]) 
-      like_j = - 0.5*t(X%*%beta[,j] -c_j - z_j)%*%diag(omega[,j])%*%(X%*%beta[,j] -c_j - z_j) + 0.5*t(z_j)%*%delta[,j]
-      logkernel = logkernel + prior_j + like_j
-      
-      for (i in 1:N){ 
-        logkernel = logkernel + polyagamma_logpdf(x = omega[i,j], h = temper, z = 0) 
-      }
-    }
-    
-    penalty = temper*kappa[1]*log(alpha[1]) + temper*(N-kappa[L])*log(alpha[L+1])
-    for(l in 2:L){
-      penalty = penalty + temper*(kappa[l]-kappa[l-1])*log(alpha[l])
-    }
-    logkernel =  logkernel + penalty
-    
-    return(logkernel)
-    
-  }
-  
-  
-  ##################################### Fitting multiple change points ####################################
-  cat("\n", "Fitting ", num_CP ," change points......")
-  
-  L = num_CP # the number of change points 
-  J = L+1 # the number of classes
-  n = dim(data)[1] # the length of the entire series
-  
-  # priors for beta (multinomial logistic regression coefficients)
-  m0 <- array(data=0, dim=c(p,J-1)) # m0[,j] is the prior mean of the jth coef vec
-  V0 <- array(data=0, dim=c(p,p,J-1)) # V0[,,j] is the prior cov. of the jth coef vec
-  V0inv <- array(data=0, dim=c(p,p,J-1)) # V0inv[,,j] is the inverse of V0[,,j]
+  # priors for beta (logistic regression coefficients)
+  m0 <- rep(0, times = p) # m0[,j] is the prior mean vector 
   sd0 <- 3 # prior standard deviation 
-  for(j in 1:(J-1)){
-    V0[,,j] <- sd0*diag(p)
-    V0inv[,,j] <- 1/sd0*diag(p)
-  }
+  V0 <- sd0*diag(p) # V0 is the prior cov. matrix
+  V0inv <- 1/sd0*diag(p) # V0inv is the prior precision matrix
   
   
   # Initialize working quantities
-  beta= matrix(0, nrow = p, ncol = J)
-  omega = matrix(0, nrow = N, ncol = J)
-  P = matrix(0, nrow = N, ncol = J)
-  if (anyNA(init_CP)){
-    # if initial CPs are NAs, use the even points as initial
-    kappa = rep(0, L)
-    for (l in 1:L){
-      kappa[l] = round(N/J) *l
-    }
+  beta = rep(0, times = p) 
+  omega = rep(0, times = N) 
+  P = rep(0, times = N) 
+
+  # Initialize a changepoint
+  if (is.na(init_CP)){
+    # if there's no user-specified initial CPs, initialize at the middle point
+    kappa = round(N/2) 
   }else{
     if (thinning == 1){
-      kappa = which(thin_idx == init_CP) 
+      kappa = init_CP
     }else{
-      kappa = c()
       # time points selected to fit the model
       thin_idx = seq_len(n)[seq_len(n) %% thinning != 0]
-      for (k in init_CP){
-        if (k %in% thin_idx){
-          # if the initial CP is selected to fit the model, use it as is. 
-          kappa = c(kappa, which(thin_idx == k))
+      if (init_CP %in% thin_idx){
+        # if the initial CP is selected to fit the model, use it as is. 
+        kappa = which(thin_idx == init_CP) #init_CP
+      }else{
+        # if the initial CP is not selected to fit the model, 
+        # pick the point closest before the initial CP in the selected points. 
+        left_idx = thin_idx[thin_idx < init_CP]
+        if (length(left_idx) > 0){
+          kappa = which(thin_idx == max(left_idx)) #max(left_idx)
         }else{
-          # if the initial CP is not selected to fit the model, 
-          # pick the point closest before the initial CP in the selected points. 
-          left_pts = thin_idx[thin_idx<k]
-          if(length(left_pts)>0){
-            kappa = c(kappa, which(thin_idx == max(left_pts)))
-          }else{
-            # if there's not points to the left of the initial CP, 
-            # (print a warning and) choose the middle point as initial.
-            # print("no points before the initial CP")
-            kappa =round(N/J) *l
-          }
+          # if there's not points to the left of the initial CP, 
+          # (print a warning and) choose the middle point as initial.
+          # print("no points before the initial CP")
+          kappa = round(N/2)
         }
       }
-
     }
   }
-  print(c(N, kappa))
-
-  out = list() # An output list
-  out$Kappa = matrix(data = rep(0, times = (num_iter-num_warmup)*L), nrow = num_iter-num_warmup, ncol = L)
-  out$Beta = array(data = rep(x = 0, times = (num_iter-num_warmup)*p*J), dim = c((num_iter-num_warmup), p, J)) 
-  out$P = array(data = rep(x = 0, times = (num_iter-num_warmup)*N*J), dim = c((num_iter-num_warmup), N, J))
-  out$AUC = matrix(0, nrow = num_iter-num_warmup, ncol = J-1)
-
+  print(c(init_CP, kappa))
   
-  #################### START of the for-loop fitting multiple change points #####################
+  # Create an output list
+  out = list()
+  out$Beta = matrix(0, nrow = num_iter-num_warmup, ncol = p)
+  out$P = matrix(0, nrow = num_iter-num_warmup, ncol = N)
+  out$Kappa = rep(0, times = num_iter - num_warmup)
+  out$AUC = rep(0, times = num_iter - num_warmup)
+  
+  
+  ######## START of the for-loop
+  start_time = Sys.time()
   pb = txtProgressBar(min = 0, max = num_iter, style = 3)
-
-  start = Sys.time()
-  
   for (iter in 1:num_iter){
-    
-    # 1. Update samples 
-    gs = GS_update(temper = 1, beta, kappa, omega, data, X, X_auc, N, L, J, m0, V0inv)
-    beta = gs$beta
-    kappa = gs$kappa
-    omega = gs$omega
-    P = gs$P
-    AUC = gs$Auc
-    original_kappa = gs$original_kappa
-    
-    
-    # 2. Store the samples in the current iteration
-    if ( iter > num_warmup){
-      out$Beta[iter-num_warmup,,] = beta
-      out$Kappa[iter-num_warmup,] = original_kappa
-      out$P[iter-num_warmup,,] = P
-      out$AUC[iter-num_warmup,] = AUC
+    ####### Update omega 
+    eta = X %*% beta
+    for (i in 1:N){
+      omega[i] = rpg.devroye(num = 1, h = 1, z = eta[i]) 
     }
     
+    ####### Update beta 
+    # V = solve(t(X)%*%diag(omega)%*%X + V0inv)
+    # delta = c(rep(-0.5, times = kappa), rep(0.5, times = N-kappa)) 
+    # m = V%*%(t(X)%*%delta + V0inv%*%m0)
+    # beta = mvrnorm(n=1, mu = m, Sigma = V)
+    delta = c(rep(-0.5, times = kappa), rep(0.5, times = N-kappa)) 
+    Z <- matrix(rnorm(p), p, 1) # A p-vector of standard normals
+    U <- chol(crossprod(x = X, y = diag(omega) %*% X) + V0inv) # Upper Cholesky factor of the inverse of V_j 
+    beta <- chol2inv(U) %*% (t(X) %*%delta + V0inv %*% m0) + backsolve(U, Z)
+    ####### Update P 
+    Phi_exp <- exp(X %*% beta) 
+    P = Phi_exp/(1+Phi_exp)
+    
+    range <- 1:(N-1)
+    lr <- length(range)
+    log_prob_vec_prop <- rep(0, lr) 
+    for(l in 1:lr){
+      log_prob_vec_prop[l] = sum(log(1-P[1:range[l]])) + sum(log(P[(range[l]+1):N])) 
+      - length(1:range[l])*log(length(1:range[l])) 
+      - length((range[l]+1):N)*log(length((range[l]+1):N))
+    } 
+    
+    prob_vec <- exp(log_prob_vec_prop - logsumexp(log_prob_vec_prop)) 
+    if(lr > 1){
+      kappa = sample(x=range, size=1, prob=prob_vec)
+    }
+    
+    ##### Compute AUC ####
+    all_idx = 1:n
+    if (thinning == 1){
+      thin_idx = all_idx
+    }else{
+      thin_idx = all_idx[all_idx%%thinning != 0]
+    }
+    original_kappa = thin_idx[kappa]
+    # Update Y based on the kappa in the entire series
+    Y = c(rep(0, times = original_kappa), rep(1, times = n - original_kappa))
+    Y_auc = Y[seq_len(length(Y)) %% thinning == 0]
+    # Update the matrix of probabilities of success based on the fitted coefficients
+    Phi_exp_auc <- exp(X_auc %*% beta) 
+    P_auc = Phi_exp_auc/(1+Phi_exp_auc)
+    # Compute AUC value
+    auc = calculate_roc_auc(Y_auc, P_auc)
+    
+    # Store samples after burn-in
+    if(iter > num_warmup){
+      out$Beta[iter-num_warmup,] = beta 
+      out$P[iter-num_warmup,] = P
+      out$Kappa[iter-num_warmup] = original_kappa
+      out$AUC[iter-num_warmup] = auc
+    }
     setTxtProgressBar(pb, iter)
   }
-  
-  
-  end = Sys.time()
-  close(pb)
-  
-  ##################### END of the for-loop fitting multiple change points #######################
-  
-  runTime = end - start
-  
+  end_time  = Sys.time()
+  close(pb) 
+
+  ######### END of the for-loop 
   if (print_outputs){
-    cat("\n" ,"The algorithm took ", format(runTime), "to run ", num_iter, 
-        "iterations, including ", num_warmup, "burn-in iterations", "to fit ", num_CP, 
-        "change points in a data set in", p, "dimensions." )
+    runTime = end_time  - start_time 
+    cat("The algorithm took ", format(runTime), "to run ", num_iter, "iterations with", 
+        num_warmup, "burn-in iterations to fit",  num_CP, 
+        "change point in a data set in", p, "dimensions.")
     cat("The output list has the structure", "\n", str(out))
   }
+  
   
   return(out)
 }
