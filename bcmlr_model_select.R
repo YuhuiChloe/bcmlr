a646fe5a4beec00d831e141d13c56c5f012dffe9
YuhuiChloe <87951710+YuhuiChloe@users.noreply.github.com>
2025-09-04 12:39:41 -0400

    Update bcmlr_model_select.R
    Complete a comment and reassure 'boolean' is a matrix

--- bcmlr_model_select.R
+++ bcmlr_model_select.R
@@ -5,7 +5,7 @@ bcmlr_model_select <- function(data, init, thinning, prior, alpha_f = 0.1, thres
               min_size = min_size, num_iter = num_iter, num_warmup = num_warmup,
               print_outputs = FALSE, print_progress = print_progress, model_selection = TRUE)
   boolean = (old_out$AUC < threshold) # TRUE if less than threshold, FALSE otherwise
-  rej = rowSums(!boolean) # Count number of FALSE in each row
+  rej = rowSums(!as.matrix(boolean)) # Count number of FALSE in each row
   tb = table(factor(rej, levels = seq_len(max_num_cp+1)-1))/length(rej)  # The levels start from 0 to max_num_cp
   num_cp = as.integer(names(tb)[which.max(tb)]) # the posterior mode is taken as the estimated num of CP
   probs = as.numeric(tb) # the posterior probabilities of number of CPs from 0 to max_num_cp
@@ -16,6 +16,6 @@ bcmlr_model_select <- function(data, init, thinning, prior, alpha_f = 0.1, thres
   out$num_cp = num_cp # estimate of the number of CPs 
   out$max_cps = old_out$Kappa # the max number of CPs from the "old output" before refitting on all the data
   out$num_cp_dist = rej # posterior distribution of the number of CPs
-  out$probs_num_cp = probs 
+  out$probs_num_cp = probs # posterior probabilities of the number of CPs
   return(out)
 }
