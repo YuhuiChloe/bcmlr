9c34ccafb5f8387ec6503ab30a3b2826b06b5732
YuhuiChloe <87951710+YuhuiChloe@users.noreply.github.com>
2025-09-11 21:27:08 -0400

    Update bcmlr_model_select.R
    Added if-statements checks for better presentation in edge cases.

--- bcmlr_model_select.R
+++ bcmlr_model_select.R
@@ -1,5 +1,8 @@
 bcmlr_model_select <- function(data, init, thinning, prior, alpha_f = 0.1, threshold = 0.5, min_size = 30, 
                                max_num_cp = 5, num_iter = 5000, num_warmup = 2500, print_progress = FALSE){
+  if (max_num_cp > (dim(data)[1]/min_size -1)){
+    stop("The value of max_num_cp exceeded the maximum possible number of changepoints under the specified min_size.")
+  }
   # Select the number of changepoints
   old_out = bcmlr(data, init = init, num_CP = max_num_cp, prior = prior, alpha_f = alpha_f, thinning = thinning,
               min_size = min_size, num_iter = num_iter, num_warmup = num_warmup,
@@ -10,12 +13,21 @@ bcmlr_model_select <- function(data, init, thinning, prior, alpha_f = 0.1, thres
   num_cp = as.integer(names(tb)[which.max(tb)]) # the posterior mode is taken as the estimated num of CP
   probs = as.numeric(tb) # the posterior probabilities of number of CPs from 0 to max_num_cp
   names(probs) = names(tb) 
-  # Refit with all the data: 
-  out = bcmlr(data, init = init, num_CP = num_cp, prior = prior, thinning = 1, min_size = min_size, num_iter = num_iter, num_warmup = num_warmup,
-              print_outputs = FALSE, print_progress = print_progress, model_selection = FALSE)
-  out$num_cp = num_cp # estimate of the number of CPs 
-  out$max_cps = old_out$Kappa # the max number of CPs from the "old output" before refitting on all the data
-  out$num_cp_dist = rej # posterior distribution of the number of CPs
-  out$probs_num_cp = probs # posterior probabilities of the number of CPs
+  if (num_cp == 0){
+    print("There is no change.")
+    out = list()
+    out$num_cp = num_cp # estimate of the number of CPs 
+    out$max_cps = old_out$Kappa # the max number of CPs from the "old output" before refitting on all the data
+    out$num_cp_dist = rej # posterior distribution of the number of CPs
+    out$probs_num_cp = probs 
+  }else{
+    # Refit with all the data: 
+    out = bcmlr(data, init = init, num_CP = num_cp, prior = prior, thinning = 1, min_size = min_size, num_iter = num_iter, num_warmup = num_warmup,
+                print_outputs = FALSE, print_progress = print_progress, model_selection = FALSE)
+    out$num_cp = num_cp # estimate of the number of CPs 
+    out$max_cps = old_out$Kappa # the max number of CPs from the "old output" before refitting on all the data
+    out$num_cp_dist = rej # posterior distribution of the number of CPs
+    out$probs_num_cp = probs 
+  }
   return(out)
 }
