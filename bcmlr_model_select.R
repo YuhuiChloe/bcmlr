e6f6c4be8fd508fbf2bebf35f409e0aa51f675d1
YuhuiChloe <87951710+YuhuiChloe@users.noreply.github.com>
2025-10-27 18:47:07 -0400

    Update bcmlr_model_select.R
    Give default values to "init" and "thinning"

--- bcmlr_model_select.R
+++ bcmlr_model_select.R
@@ -1,4 +1,4 @@
-bcmlr_model_select <- function(data, init, thinning, prior, alpha_f = 0.1, threshold = 0.5, min_size = 30, 
+bcmlr_model_select <- function(data, init = "even", thinning = 5, prior_beta = "Gaussian", prior_kappa = "default", alpha_f = 0.1, threshold = 0.5, min_size = 30, 
                                max_num_cp = 5, num_iter = 5000, num_warmup = 2500, print_progress = FALSE){
   if (!is.matrix(data)){
     stop("Please convert your data to a matrix. If you have factor variables, apply a dummification or one-hot encoding.")
@@ -7,7 +7,7 @@ bcmlr_model_select <- function(data, init, thinning, prior, alpha_f = 0.1, thres
     stop("The value of max_num_cp exceeded the maximum possible number of changepoints under the specified min_size.")
   }
   # Select the number of changepoints
-  old_out = bcmlr(data, init = init, num_CP = max_num_cp, prior = prior, alpha_f = alpha_f, thinning = thinning,
+  old_out = bcmlr(data, init = init, num_CP = max_num_cp, prior_beta = prior_beta, prior_kappa = prior_kappa, alpha_f = alpha_f, thinning = thinning,
               min_size = min_size, num_iter = num_iter, num_warmup = num_warmup,
               print_outputs = FALSE, print_progress = print_progress, model_selection = TRUE)
   boolean = (old_out$AUC < threshold) # TRUE if less than threshold, FALSE otherwise
@@ -25,7 +25,7 @@ bcmlr_model_select <- function(data, init, thinning, prior, alpha_f = 0.1, thres
     out$probs_num_cp = probs 
   }else{
     # Refit with all the data: 
-    out = bcmlr(data, init = init, num_CP = num_cp, prior = prior, thinning = 1, min_size = min_size, num_iter = num_iter, num_warmup = num_warmup,
+    out = bcmlr(data, init = init, num_CP = num_cp, prior_beta = prior_beta, prior_kappa = prior_kappa, thinning = 1, min_size = min_size, num_iter = num_iter, num_warmup = num_warmup,
                 print_outputs = FALSE, print_progress = print_progress, model_selection = FALSE)
     out$num_cp = num_cp # estimate of the number of CPs 
     out$max_cps = old_out$Kappa_mode # the max number of posterior modes from the "old output" before refitting on all the data
