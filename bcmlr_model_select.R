2ba604d26edb4f12faab2bc6fdb6a96a1b82c288
YuhuiChloe <87951710+YuhuiChloe@users.noreply.github.com>
2026-04-12 18:32:28 -0400

    Update bcmlr_model_select.R

--- bcmlr_model_select.R
+++ bcmlr_model_select.R
@@ -10,7 +10,7 @@ bcmlr_model_select <- function(data, init = "even", thinning = 5, prior_beta = "
   old_out = bcmlr(data, init = init, num_CP = max_num_cp, prior_beta = prior_beta, prior_kappa = prior_kappa, alpha_f = alpha_f, thinning = thinning,
               min_size = min_size, num_iter = num_iter, num_warmup = num_warmup,
               print_outputs = FALSE, print_progress = print_progress, model_selection = TRUE)
-  boolean = (old_out$AUC < threshold) # TRUE if less than threshold, FALSE otherwise
+  boolean = (old_out$AUC <= threshold) # TRUE if less than threshold, FALSE otherwise
   rej = rowSums(!as.matrix(boolean)) # Count number of FALSE in each row
   tb = table(factor(rej, levels = seq_len(max_num_cp+1)-1))/length(rej)  # The levels start from 0 to max_num_cp
   num_cp = as.integer(names(tb)[which.max(tb)]) # the posterior mode is taken as the estimated num of CP
